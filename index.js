17da91e67d91a6301a4c817a2c1e954b86e0b9d6
Tebogo Seanego <50863472+VeranoSA@users.noreply.github.com>
2021-09-14 12:58:21 +0200

    Pizza final

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const express = require('express');
-
+var session = require('express-session')
 const app = express();
 const exphbs  = require('express-handlebars');
-
+const helperfunction = require('./perfect-pizza');
+const helperfunc = helperfunction();
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static('public'));
 
-// add your routes here...
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
+
 app.get('/', function(req, res){
 	res.render('index');
+});
+// add your routes here...
+app.post('/addToCart', function(req, res){
+
+	res.render('index', {
+		result: helperfunc.addToCart(req.body.button)
+	});
 });
 //a get route to show a list of pizzas
 app.get('/pizzas', function(req, res) {
@@ -36,7 +44,60 @@ app.get('/pizza/edit/:pizza_id', function(req, res){
 app.post('/pizza/edit', function(req, res) {
 	res.redirect("/pizzas");
 });
+app.post('/pizza/modify', function(req, res) {
+	res.render('pizza/viewcart', {
+		result: helperfunc.modifyVals(req.body.button)
+	});
+});
+
+app.get('/viewcart', function(req, res) {
+	var grandTotal = helperfunc.grandTotal();
+	if (!req.session.grandTotal) {
+		req.session.grandTotal = {due: true, Id:1, total:grandTotal}
+	} else {
+		req.session.grandTotal = {Id:req.session.grandTotal.Id, due:req.session.grandTotal.due, total:grandTotal}
+		
+	}
+	//console.log(req.session.grandTotal);
+	res.render('pizza/viewcart', {
+		result: helperfunc.cartTotals(),
+		sess: req.session.grandTotal,
+	});
+});
+
+app.get('/pay', function(req, res) {
+	var grandTotal = helperfunc.grandTotal();
+	if (!req.session.grandTotal) {
+		req.session.grandTotal = {Id:req.session.grandTotal.Id, due:req.session.grandTotal.due, total:grandTotal, paid:true}
+	} else {
+		req.session.grandTotal = {Id:req.session.grandTotal.Id, due:req.session.grandTotal.due, total:grandTotal, paid:req.session.grandTotal.paid}
+	}
+	res.render('pizza/viewcart', {
+		sess: req.session.grandTotal
+	});
+	//console.log(req.session.grandTotal);
+});
+
+app.get('/paid', function(req, res) {
+	var grandTotal = helperfunc.grandTotal();
+	if (!req.session.grandTotal) {
+		req.session.grandTotal = {due: true, Id:1, total:grandTotal}
+	} else {
+		req.session.grandTotal = {Id:req.session.grandTotal.Id, total:grandTotal}
+	}
+	res.render('pizza/viewcart', {
+		sess: req.session.grandTotal
+	});
+});
+
+app.get('/checkout', function(req, res) {
+	res.render('pizza/order', {
+		result: req.session.grandTotal,
+	});
+});
+
 const PORT = process.env.PORT || 7008;
 app.listen(PORT, function(){
 	console.log('started');
 });
+
--- perfect-pizza.js
+++ perfect-pizza.js
@@ -1,36 +1,79 @@
-module.exports = function () {
+module.exports = function pizzaPerfect() {
 
     var smallTotal = 0;
     var mediumTotal = 0;
     var largeTotal = 0;
+    var smallQuantity = 0;
+    var mediumQuantity = 0;
+    var largeQuantity = 0;
+    
 
-    function getTotal(pizza) {
 
+    function addToCart(pizza) {
         if (pizza === "small") {
             smallTotal += 35.00;
-        }
-        else if (pizza === "meduim") {
+            smallQuantity+=1;
+            return cartTotals();
+        } else if (pizza === "medium") {
             mediumTotal += 65.00;
-        }
-
-        else if (pizza === "large") {
-            largeTotal += 85.00
+            mediumQuantity+=1;
+            return cartTotals();
+        } else if (pizza === "large") {
+            largeTotal += 85.00;
+            largeQuantity+=1;
+            return cartTotals();
         }
     }
 
     function grandTotal() {
-        return smallTotal + mediumTotal + largeTotal;
-
+        return smallTotal + mediumTotal+ largeTotal;
     }
-    function totals() {
-        return smallTotal.toFixed(2),
-            mediumTotal.toFixed(2),
-            largeTotal.toFixed(2)
+
+    function cartTotals() {
+        return {
+            smallTotal: smallTotal.toFixed(2),
+            smallQuantity: smallQuantity,
+            mediumTotal: mediumTotal.toFixed(2),
+            mediumQuantity: mediumQuantity,
+            largeTotal: largeTotal.toFixed(2),
+            largeQuantity: largeQuantity,
+            cartTotal: grandTotal().toFixed(2)
+        };
+    }
+
+    function modifyVals(param) {
+        if (param === "smallAdd") {
+            smallTotal += 35.00;
+            smallQuantity+=1;
+        } else if (param === "smallSubs") {
+            if(smallQuantity !== 0) {
+                smallTotal -= 35.00;
+                smallQuantity-=1;
+            }
+        } else if (param === "mediumAdd") {
+            mediumTotal += 65.00;
+            mediumQuantity+=1;
+        } else if (param === "mediumSubs") {
+            if(mediumQuantity !== 0) {
+                mediumTotal -= 65.00;
+                mediumQuantity-=1;
+            }
+        } else if (param === "largeAdd") {
+            largeTotal += 85.00;
+            largeQuantity+=1;
+        } else if (param === "largeSubs") {
+            if(largeQuantity !== 0) {
+                largeTotal -= 85.00;
+                largeQuantity-=1;
+            }
+        }
+        return cartTotals();
     }
 
     return {
-        getTotal,
+        addToCart,
         grandTotal,
-        totals
+        cartTotals,
+        modifyVals,
     }
-}
+}   
